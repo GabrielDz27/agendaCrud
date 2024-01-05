065d11c3ac29f5198958faaabada7d6cfa0a2492
Gabriel Dziecinny Ferreira <110194151+GabrielDz27@users.noreply.github.com>
2024-01-04 21:30:32 -0300

    Add files via upload

---
+++ agendaCrud1/resources/rodape.php
@@ -0,0 +1,6 @@
+</div>
+<script type="text/javascript" src="./lib/jquery/js/jquery-3.1.1.js"></script>
+<script type="text/javascript" src="./lib/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./common/js/crud.js"></script>
+</body>
+</html>
